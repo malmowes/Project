da0e0f793ff8a9180e55b2675137eee6e1055c7e
wes <malmowes@hotmail.com>
2024-11-14 09:13:10 +0100

    Initial Commit

---
+++ v2b/v2bApp.swift
@@ -0,0 +1,17 @@
+//
+//  v2bApp.swift
+//  v2b
+//
+//  Created by applwes on 2024-11-14.
+//
+
+import SwiftUI
+
+@main
+struct v2bApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
