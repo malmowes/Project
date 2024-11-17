6830a16cd7b8fe7df0e812c4e03f883e51153971
wes <malmowes@hotmail.com>
2024-11-18 00:18:50 +0100

    nav

---
+++ v2b/personrow.swift
@@ -0,0 +1,18 @@
+//
+//  personrow.swift
+//  v2b
+//
+//  Created by applwes on 2024-11-17.
+//
+
+import SwiftUI
+
+struct personrow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    personrow()
+}
---
+++ v2b/readmore.swift
@@ -0,0 +1,18 @@
+//
+//  readmore.swift
+//  v2b
+//
+//  Created by applwes on 2024-11-18.
+//
+
+import SwiftUI
+
+struct readmore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    readmore()
+}
