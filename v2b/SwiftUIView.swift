976cc41e212b9059c6a4b23fd67609fb1f3d9504
wes <malmowes@hotmail.com>
2024-11-17 22:47:41 +0100

    hej

---
+++ v2b/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  v2b
+//
+//  Created by applwes on 2024-11-14.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
